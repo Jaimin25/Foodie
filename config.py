968a90a7391782374419c8a0ecb4c49913e180f3
Jaimin25 <jaiminchovatia25123@gmail.com>
2022-05-30 11:35:08 +0530

    initial commit

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 DATABASE_CREDENTIALS = {
-    "host": "database-2.cn35g8v4ickt.us-east-2.rds.amazonaws.com",
-    "user": "postgres",
-    "password": "Jac25123",
-    "database": "postgres",
+    "host": "ec2-3-211-221-185.compute-1.amazonaws.com",
+    "user": "znpeqvfkxeahux",
+    "password": "6f1909e38424ec36858de7b3395e5a2d37903b69a4a0ad14996aa0cfe6b761df",
+    "database": "d3r2eicpt1t4fl",
     "min_size": 2,
     "max_size": 4
 }
