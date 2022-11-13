c3746f2a8318b0ab0e4744f546f39830ea18b1f7
Jaimin25 <jaiminchovatia25123@gmail.com>
2022-11-13 10:06:54 +0530

    initial commit

--- cogs/upgrades.py
+++ cogs/upgrades.py
@@ -13,7 +13,6 @@ class Upgrades(commands.Cog):
         self.client = client
 
     @app_commands.command(description="Check your upgrades")
-    @app_commands.guilds(discord.Object(955385300513878026))
     @app_commands.checks.cooldown(1, 5.0, key=lambda i: (i.guild_id, i.user.id))
     @app_commands.describe(
         page='Page number = 1, 2'
@@ -129,7 +128,6 @@ class Upgrades(commands.Cog):
         return sum
 
     @app_commands.command(description="Buy items for kitchen upgrades")
-    @app_commands.guilds(discord.Object(955385300513878026))
     @app_commands.checks.cooldown(1, 5.0, key=lambda i: (i.guild_id, i.user.id))
     @app_commands.describe(
         amount='Amount/Max',
@@ -152,7 +150,6 @@ class Upgrades(commands.Cog):
         await interaction.response.send_message(embed=success_embed)
 
     @app_commands.command(description="Hire employees for staff upgrades ")
-    @app_commands.guilds(discord.Object(955385300513878026))
     @app_commands.checks.cooldown(1, 5.0, key=lambda i: (i.guild_id, i.user.id))
     @app_commands.describe(
         amount='Amount/Max',
