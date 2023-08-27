d7d9848f6f7843d4a15efb44a9ef67e03aa17529
Jaimin25 <65119631+Jaimin25@users.noreply.github.com>
2023-08-27 10:44:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 DATABASE_CREDENTIALS = {
-    "host": "ec2-3-211-221-185.compute-1.amazonaws.com",
-    "user": "znpeqvfkxeahux",
-    "password": "6f1909e38424ec36858de7b3395e5a2d37903b69a4a0ad14996aa0cfe6b761df",
-    "database": "d3r2eicpt1t4fl",
+    "host": "HOST-NAME",
+    "user": "USER-NAME",
+    "password": "PASSWORD",
+    "database": "DATABASE-NAME",
     "min_size": 2,
     "max_size": 4
 }
-TOKEN = "OTY0ODgxNzI5OTIxODkyNDAy.YlrGIw.h7VbEJZAkGONtham8M9iNtidtHk"
+TOKEN = "YOUR-TOKEN"
